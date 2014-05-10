f70d990e761634a4b923dda1552c2d6f73118f69
A. Karl Kornel <karl@kornel.name>
2014-05-09 22:02:57 -0700

    Only define skippable tests in Python 2.7+.

--- t/logger.py
+++ t/logger.py
@@ -5,6 +5,7 @@ Created on May 9, 2014
 '''
 import unittest
 import logging
+from sys import version_info
 
 # If we're being run directly, then we need to add the parent dir to path
 try:
@@ -13,6 +14,14 @@ except:
     from sys import path
     path.append('..')
     from k2ksm import logger
+    
+# Get the version number.  Test skipping came in 2.7.
+majorVer, minorVer = version_info[:2]
+canSkip = False
+if (   (majorVer >= 3)
+    or (minorVer >= 7)
+       ):
+    canSkip = True
 
 
 class K2LoggerTests(unittest.TestCase):
@@ -23,10 +32,12 @@ class K2LoggerTests(unittest.TestCase):
         l = logger.K2Logger('Karl')
         self.assertIsInstance(l, logger.K2Logger)
     
-    @unittest.skip("I don't know what object won't give me a str() output")
-    def test_create_badName(self):
-        # Creating a logger with something non-str()able should fail
-        pass
+    # Skipping is allowed in 3+ or 2.7+
+    if canSkip:
+        @unittest.skip("I don't know what object won't give me a str() output")
+        def test_create_badName(self):
+            # Creating a logger with something non-str()able should fail
+            pass
 
     @unittest.expectedFailure
     def test_namePrefix_set(self):
@@ -65,10 +76,14 @@ class K2LoggerTests(unittest.TestCase):
         
         
 # List the tests and create a test suite, for use by the top-level test script.
-tests = ('test_create', 'test_create_badName',
+tests = ('test_create',
          'test_namePrefix_set', 'test_namePrefix_delete',
          'test_logger', 'test_logger_set', 'test_logger_delete')
-K2LoggerTestSuite = unittest.TestSuite(map(K2LoggerTests, tests))
+skippedTests = ('test_create_badName')
+if canSkip:
+    K2LoggerTestSuite = unittest.TestSuite(map(K2LoggerTests, tests, skippedTests))
+else:
+    K2LoggerTestSuite = unittest.TestSuite(map(K2LoggerTests, tests))
 
     
 # Allow this set of test cases to be run by themselves.
