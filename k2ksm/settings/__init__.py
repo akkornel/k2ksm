b9dc9f4bc7d762a2c04aa67f59e8569739dec5ae
A. Karl Kornel <karl@kornel.name>
2014-05-16 01:26:51 -0700

    We can't delete a class member, so just replace it instead.

--- k2ksm/settings/__init__.py
+++ k2ksm/settings/__init__.py
@@ -338,7 +338,7 @@ class K2Settings(object):
             self.logger.warning('Module ' + moduleName
                                 + ' had settings defined, but ' + moduleName
                                 + ' was not loaded.')
-        del self.__unusedSettings
+        self.__unusedSettings = []
         self.finalized = True
         
         
